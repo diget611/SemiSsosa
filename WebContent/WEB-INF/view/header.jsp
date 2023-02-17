4930f30fb288fc176054a95e1299732b54099c3a
diget611 <diget611@naver.com>
2023-02-17 14:15:23 +0900

    게시판 기능 추가 중

--- WebContent/WEB-INF/view/header.jsp
+++ WebContent/WEB-INF/view/header.jsp
@@ -3,6 +3,12 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <div>
+	<button type="button" class="btn title">title</button>
+</div>
+<div>
+	<button type="button" class="btn board">board1</button>
+</div>
+<div>
 	<c:choose>
 		<c:when test="${empty login }">
 			<button type="button" class="btn login">로그인</button>
@@ -17,6 +23,8 @@
 <script>
 	$(".btn.login").on("click", clickLogin);
 	$(".btn.logout").on("click", clickLogout);
+	$(".btn.title").on("click", toMain);
+	$(".btn.board").on("click", toBoard);
 	
 	function clickLogin() {
 		console.log("로그인 버튼 클릭");
@@ -25,5 +33,11 @@
 	function clickLogout() {
 		console.log("로그아웃 버튼 클릭");
 		location.href="<%=request.getContextPath()%>/logout";
+	}
+	function toMain() {
+		location.href="<%=request.getContextPath()%>/"
+	}
+	function toBoard() {
+		location.href="<%=request.getContextPath()%>/board"
 	}
 </script>
---
+++ src/board/controller/BoardController.java
@@ -0,0 +1,36 @@
+package board.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/*
+ * 게시판 테이블 명 BOARD_TEMP로 진행. 변경할 것
+ */
+
+/**
+ * Servlet implementation class BoardController
+ */
+@WebServlet("/board")
+public class BoardController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BoardController() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/view/board/board.jsp").forward(request, response);
+	}
+}
